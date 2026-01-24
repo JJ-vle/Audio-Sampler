e4bf654ca726466dca27050eb023f0f1ec4536db
JJ-vle <jeanjacquesviale.txt@gmail.com>
2026-01-24 14:36:27 +0100

    fix httpclient angular

--- AngularFrontEnd/src/app/app.ts
+++ AngularFrontEnd/src/app/app.ts
@@ -5,13 +5,12 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-//import { Presets } from './presets/presets';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink,
     MatButtonModule, MatDividerModule, MatIconModule, 
-    MatToolbarModule/*, Presets*/],
+    MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
